4b3da043abc9811288e86bcb451a0b34105b1fb0
Nahue <nahue.p.sanz@gmail.com>
2023-07-03 10:26:44 -0300

    agregamos bootstrap al proyecto y probamos menu basico

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,17 +3,16 @@ import Navbar from "./components/NavBar/NavBar";
 import Banner from "./components/Banner/Banner";
 import ListingGrid from "./components/ListingGrid/ListingGrid";
 import Footer from "./components/Footer/Footer";
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const App = () => {
   return (
     <div className="app">
       <Banner />
       <Navbar />
-      <Routes>
-        <Route element={<ListingGrid />}></Route>
-      </Routes>
+      <ListingGrid />
       <Footer />
     </div>
   );
--- Client/src/Components/ListingGrid/ListingGrid.jsx
+++ Client/src/Components/ListingGrid/ListingGrid.jsx
@@ -1,7 +1,10 @@
+import ListingCard from "../ListingCard/ListingCard";
+
 const listingGrid = () => {
   return (
     <div>
-      <h1>soy el listingGrid!</h1>
+      <h1>Aqui van las Cards de las publicaciones de las casas</h1>
+      <ListingCard />
     </div>
   );
 };
