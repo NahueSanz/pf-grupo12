00afa602262fb1c121d859e8cedaf89e53b121ce
Ivan Munoz <ivmunoz9@gmail.com>
2023-07-07 04:03:35 -0300

    Footer y pequeños arreglos

--- Client/src/Components/Inc/Footer/Footer.jsx
+++ Client/src/Components/Inc/Footer/Footer.jsx
@@ -1,8 +1,57 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import styles from "./Footer.module.css";
+
 const Footer = () => {
   return (
-    <div>
-
-    </div>
+    <footer className={`${styles.footer} bg-dark text-light py-4`}>
+      <Container>
+        <Row>
+          <Col xs={12} md={4}>
+            <h5>Links</h5>
+            <ul className="list-unstyled">{/*CAMBIAR POR ETIQUETAS LINK*/ }
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={12} md={4}>
+            <h5>Social Media</h5> {/*CAMBIAR POR BOOTSTRAP ICONS*/ }
+            <ul className="list-unstyled">
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={12} md={4}>
+            <h5>Contact Us</h5>
+            <ul className="list-unstyled">
+              <li>Email: example@example.com</li>
+              <li>Phone: 123-456-7890</li>
+              <li>Address: 123 Street, City, Country</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
--- Client/src/Components/Views/PropertyForm/PropertyForm.jsx
+++ Client/src/Components/Views/PropertyForm/PropertyForm.jsx
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import styles from './PropertyForm.module.css';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import styles from "./PropertyForm.module.css";
 
 const PropertyForm = () => {
   const [formData, setFormData] = useState({
-    title: '',
-    owner: '',
-    type: '',
-    address: '',
-    country: '',
+    title: "",
+    owner: "",
+    type: "",
+    address: "",
+    country: "",
     guests: 0,
     enabled: true,
     score: 0,
     price: 0,
     review: [],
-    description: '',
-    startDate: '',
-    endDate: '',
+    description: "",
+    startDate: "",
+    endDate: "",
   });
 
-  const handleChange = (e) => { //QUIZA SEA MEJOR HACER UN HANDER POR CADA ITEM
+  const handleChange = (e) => {
+    //QUIZA SEA MEJOR HACER UN HANDER POR CADA ITEM
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -47,7 +48,8 @@ const PropertyForm = () => {
         />
       </Form.Group>
 
-      <Form.Group controlId="formOwner">{/*CAMBIAR FORM POR DATOS DEL USIARIO*/}
+      <Form.Group controlId="formOwner">
+        {/*CAMBIAR FORM POR DATOS DEL USIARIO*/}
         <Form.Label>Owner</Form.Label>
         <Form.Control
           type="text"
@@ -75,33 +77,36 @@ const PropertyForm = () => {
         </Form.Control>
       </Form.Group>
 
-      <Form.Group controlId="formAddress">
-        <Form.Label>Address</Form.Label>
-        <Form.Control
-          type="text"
-          name="address"
-          value={formData.address}
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
+      <div className={styles.inline}>
+        <Form.Group controlId="formType" className={styles.inlineElement}>
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            as="select"
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select country</option>
+            {/*CAMBIAR LUEGO */}
+            <option value="Argentina">Argentina</option>
+            <option value="Brasil">Brasil</option>
+            <option value="Chile">Chile</option>
+            <option value="Uruguay">Uruguay</option>
+          </Form.Control>
+        </Form.Group>
 
-      <Form.Group controlId="formType">
-        <Form.Label>Country</Form.Label>
-        <Form.Control
-          as="select"
-          name="country"
-          value={formData.country}
-          onChange={handleChange}
-          required
-        >
-          <option value="">Select country</option>{/*CAMBIAR LUEGO */}
-          <option value="Argentina">Argentina</option>
-          <option value="Brasil">Brasil</option>
-          <option value="Chile">Chile</option>
-          <option value="Uruguay">Uruguay</option>
-        </Form.Control>
-      </Form.Group>
+        <Form.Group controlId="formAddress" className={styles.inlineElement}>
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+      </div>
 
       <Form.Group controlId="formGuests">
         <Form.Label>Guests</Form.Label>
@@ -137,7 +142,9 @@ const PropertyForm = () => {
         />
       </Form.Group>
 
-      <Form.Group controlId="formStartDate">
+    <div className={styles.inline}>
+
+      <Form.Group controlId="formStartDate" className={styles.inlineElement}>
         <Form.Label>Start Date</Form.Label>
         <Form.Control
           type="date"
@@ -148,7 +155,7 @@ const PropertyForm = () => {
         />
       </Form.Group>
 
-      <Form.Group controlId="formEndDate">
+      <Form.Group controlId="formEndDate" className={styles.inlineElement}>
         <Form.Label>End Date</Form.Label>
         <Form.Control
           type="date"
@@ -158,6 +165,8 @@ const PropertyForm = () => {
           required
         />
       </Form.Group>
+    </div>
+      
 
       <Button variant="primary" type="submit">
         Submit
