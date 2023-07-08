cb18198ad1cfa9b0f97faa351b01e10318112e91
Edinson <edisonprise@hotmail.com>
2023-07-08 11:53:31 -0500

    landingEdinson

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,28 +1,29 @@
 // import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import DetailPropertyPage from "./Components/Views/DetailProperty/DetailPropertyPage";
-import HomePage from './Components/Views/Home/HomePage';
-import PropertyForm from './Components/Views/PropertyForm/PropertyForm';
-import { useLocation } from 'react-router-dom';
-import NavBar from './Components/Inc/NavBar/NavBar';
-import Footer from './Components/Inc/Footer/Footer';
+import HomePage from "./Components/Views/Home/HomePage";
+import PropertyForm from "./Components/Views/PropertyForm/PropertyForm";
+import { useLocation } from "react-router-dom";
+import NavBar from "./Components/Inc/NavBar/NavBar";
+import Footer from "./Components/Inc/Footer/Footer";
+import Landing from "./Components/Views/Landing/Landing";
 
 const App = () => {
   const location = useLocation();
 
-
   return (
     <div className="app">
-      {location.pathname !== '/' && <NavBar/>}
-        <Routes>
-          <Route path='/home' element={<HomePage/>}/>
-          <Route path='/rooms/:id' element={<DetailPropertyPage/>}/>
-          <Route path='/become-a-host' element={<PropertyForm/>}/>
-        </Routes>
-        {location.pathname !== '/' && <Footer/>} 
+      {location.pathname !== "/" && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/rooms/:id" element={<DetailPropertyPage />} />
+        <Route path="/become-a-host" element={<PropertyForm />} />
+      </Routes>
+      {location.pathname !== "/" && <Footer />}
     </div>
   );
 };
--- Client/src/Components/Inc/Card/Card.jsx
+++ Client/src/Components/Inc/Card/Card.jsx
@@ -1,22 +1,31 @@
-import style from "./Card.module.css"
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-function CardComponent({id,image,country,description,startDate,endDate,price}) {
+import style from "./Card.module.css";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+function CardComponent({
+  id,
+  image,
+  country,
+  description,
+  startDate,
+  endDate,
+  price,
+}) {
   return (
-    <Card className={style.card} style={{ width: '17rem', height: '25rem' }}>
+    <Card className={style.card} style={{ width: "17rem", height: "25rem" }}>
       <Card.Img className={style.img} variant="top" src={image} />
       <Card.Body className={style.cardBody}>
-          <div className={style.contanerTittle}>
-          <Link className={style.link} to={`/rooms/${id}`} >
-          <h5>{country}</h5>
+        <div className={style.contanerTittle}>
+          <Link className={style.link} to={`/rooms/${id}`}>
+            <h5>{country}</h5>
           </Link>
           <span>☆ 4,55</span>
-          </div>
-    
-            <p>{description}</p>
-            <p>{startDate} al {endDate}</p>
-            <h6>${price} USD noche</h6>
-   
+        </div>
+
+        <p>{description}</p>
+        <p>
+          {startDate} al {endDate}
+        </p>
+        <h6>${price} USD noche</h6>
       </Card.Body>
     </Card>
   );
