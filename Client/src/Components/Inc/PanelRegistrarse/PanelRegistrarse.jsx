29c946688352db37ee05abe2f92fd6e9a6255e97
MikaelaMonroy <mika_monroy00@hotmail.com>
2023-07-14 02:13:16 -0300

    validaciones de autenticacion

--- Client/src/Components/Inc/PanelRegistrarse/PanelRegistrarse.jsx
+++ Client/src/Components/Inc/PanelRegistrarse/PanelRegistrarse.jsx
@@ -2,63 +2,72 @@ import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
-import firebaseApp from '../../../fb';
-import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import style from './PanelRegistrarse.module.css';
+import Alert from 'react-bootstrap/Alert';
 import axios from 'axios';
 
-const auth = getAuth(firebaseApp);
-const gProvider = new GoogleAuthProvider();
 
 function PanelRegistrarse() {
+  const redVariant = 'danger';
   const [show, setShow] = useState(false);
-  const [showPassword, setShowPassword] = useState(false); // Nuevo estado para controlar la visibilidad de la contraseña
+  const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
+  const [error, setError] = useState(null);
 
-
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+    setError(null); 
+    setFormData({
+      email: '',
+      password: '',
+    });
+  };
   const handleShow = () => setShow(true);
 
-  const loginGoogle = () => {
-    signInWithRedirect(auth, gProvider);
-  };
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    console.log(formData)
+    console.log(formData);
+  
+    // CREAR VALIDACIONES
+  
+   try{
+    const response = await axios.post(`http://localhost:3001/public/register`, formData)
 
-    //CREAR VALIDACIONES
-
-      axios.post(`http://localhost:3001/public/register`,formData)
-      .then(res=>{
-        console.log(res.data);
-        alert("Created user")});
-      
-      setFormData({
-        email: '',
-        password: '',
-      });
-
+    console.log(response.data);
+    alert('Created user');
+    setShow(false);
+    setFormData({
+      email: '',
+      password: '',
+    });
+    setError(null);
+  
+   }
+      catch(error){
+          const errorMessage = error.response.data.error;
+          setError(errorMessage); 
+        
+      }
 
   };
-useEffect(()=> console.log(formData), [formData])
+
+
 
   const handleChange = (e) => {
-
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
     });
-
-  }
+  };
 
   return (
     <>
@@ -70,53 +79,49 @@ useEffect(()=> console.log(formData), [formData])
         <Modal.Header closeButton>
           <Modal.Title>Registrarse</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Form  onSubmit={handleSubmit}>
+        <Modal.Body className={style.body}>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-            
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange}/>
+              <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} value={formData.email} />
               <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
+              
               </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control
-                type={showPassword ? 'text' : 'password'} // Mostrar la contraseña si showPassword es true
+                type={showPassword ? 'text' : 'password'}
                 placeholder="Password"
                 name="password"
                 onChange={handleChange}
+                value={formData.password}
               />
               <Form.Check
                 type="checkbox"
-                label="Show password" // Etiqueta para mostrar/ocultar la contraseña
-                onChange={togglePasswordVisibility} // Cambiar el estado de showPassword al hacer clic en la casilla de verificación
+                label="Show password"
+                onChange={togglePasswordVisibility}
               />
+               
             </Form.Group>
+            {error && (
+                  <Alert variant={redVariant}>
+                    {error}
+                  </Alert>
+                )}
 
-            <Button className={style.googleBtn} onClick={loginGoogle}>
-              <picture>
-                <img
-                  src="https://entredichos.trabajosocial.unlp.edu.ar/wp-content/uploads/sites/6/2016/12/Google_-G-_Logo.svg_.png"
-                  alt="logo-google"
-                />
-              </picture>
-              <span>Registrarse con Google</span>
-            </Button>
+       
             <Button type="submit" variant="primary">
-            Save Changes
-          </Button>
+              Save Changes
+            </Button>
           </Form>
         </Modal.Body>
 
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-     
+
         </Modal.Footer>
+
       </Modal>
     </>
   );
