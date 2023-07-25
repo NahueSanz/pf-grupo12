407e64062888c2b69625d9c78c722ad211d4013d
MikaelaMonroy <mika_monroy00@hotmail.com>
2023-07-25 00:01:46 -0300

    modificaciones en el promedio de Card

--- Client/src/Components/Inc/Comentarios/Comentarios.jsx
+++ Client/src/Components/Inc/Comentarios/Comentarios.jsx
@@ -2,6 +2,8 @@ import Card from 'react-bootstrap/Card';
 import style from './Comentarios.module.css'
 
 function Comentarios({user, review, score}) {
+
+
   return (
     <Card style={{ width: '18rem' }} className={style.container}>
       <Card.Body>
@@ -10,7 +12,7 @@ function Comentarios({user, review, score}) {
         <Card.Text >
         
        
-          <h5>{user.name} {user.lastname}</h5>
+          <h5>{user?.name} {user?.lastname}</h5>
      
           <h3>{score}</h3>
           <p>{review}</p>
